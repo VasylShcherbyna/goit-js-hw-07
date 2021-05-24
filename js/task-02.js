f2facd8c63ff8e02d02d7359207647aaeac0b347
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-05-24 10:53:53 +0200

    task - 02js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const element = document.querySelector('#ingredients');
+const gredient = gradients => {
+  const li = document.createElement('li');
+  li.textContent = gradients;
+  return li;
+};
+const arr = ingredients.map(gredient);
+element.append(...arr);
